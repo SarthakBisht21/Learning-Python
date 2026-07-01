36ec988775d5b64f31f9a30a90632756d0a0dae2
SarthakBisht21 <25113cn313@glbitm.ac.in>
2026-07-01 20:44:47 +0530

    First Python Code

--- 001.py
+++ 001.py
@@ -1,1 +1,17 @@
+#printing single line.
 print("This is my first python program.")
+
+#printning a no..
+print(980)
+print(98.87)
+
+"""MULTI
+   LINE
+   COMMENT!"""
+#    MULTI
+#    LINE
+#    COMMENT!
+
+print('''This is 
+to print
+multiple lines.''')
