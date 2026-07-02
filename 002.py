ae6514702da9c49e5cb9bac9650aca847af670f3
SarthakBisht21 <25113cn313@glbitm.ac.in>
2026-07-02 15:14:22 +0530

    Learning Python

---
+++ 002.py
@@ -0,0 +1,15 @@
+a = int(input("Enter the first no.:"))
+b = int(input("Enter the second no.:"))
+
+print("sum is:",a+b)
+
+print("average is:",(a+b)/2)
+
+print(type(a+b))
+
+print("The remainder when",a,"is divided by",b,"is",a%b)
+
+print(a,"is bigger than",b) if a>b else print(b,"is bigger than ",a)
+
+print("The square of",a,"is",a*a)
+print("The square of",b,"is",b*b)
---
+++ 003.py
@@ -0,0 +1,13 @@
+name = "SARTHAK"
+
+new_name1 = name[0:5]
+new_name2 = name[-7:-2]
+new_name3 = name[::2]
+new_name4 = name[:5]
+new_name5 = name[3:]
+
+print(len(name))
+print(name.endswith("THAK"))
+print(name.endswith("thak"))
+print(name.startswith("SAR"))
+print(name.capitalize())
